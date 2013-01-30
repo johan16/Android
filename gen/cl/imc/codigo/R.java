52e331cd4ec4b0f4f36b4b744bf347be53df6020
Mauro Bahamondes <maurobahamondes@gmail.com>
2013-01-29 21:48:18 -0300

    boton volver

--- gen/cl/imc/codigo/R.java
+++ gen/cl/imc/codigo/R.java
@@ -16,7 +16,8 @@ public final class R {
     public static final class id {
         public static final int btnCalcular=0x7f070002;
         public static final int btnResultado=0x7f070003;
-        public static final int menu_settings=0x7f070005;
+        public static final int btnVolver=0x7f070005;
+        public static final int menu_settings=0x7f070006;
         public static final int tvResultado=0x7f070004;
         public static final int txtAltura=0x7f070001;
         public static final int txtPeso=0x7f070000;
@@ -32,6 +33,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int btnCalcular=0x7f040005;
         public static final int btnResultado=0x7f040006;
+        public static final int btnVolver=0x7f040007;
         public static final int hello_world=0x7f040001;
         public static final int hintAltura=0x7f040004;
         public static final int hintPeso=0x7f040003;
--- src/cl/imc/codigo/ResultadoActivity.java
+++ src/cl/imc/codigo/ResultadoActivity.java
@@ -1,7 +1,10 @@
 package cl.imc.codigo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ResultadoActivity extends Activity{
 	@Override
@@ -10,6 +13,22 @@ public class ResultadoActivity extends Activity{
 		
 		//Asignar layout
 		this.setContentView(R.layout.layout_resultado);
+		Button btnVolver = (Button) this.findViewById(R.id.btnVolver);
+		
+		btnVolver.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				volverInicio();
+			}
+		});
+	}
+
+	protected void volverInicio() {
+		// TODO Auto-generated method stub
+		Intent i = new Intent(this, ImcActivity.class);
+		this.startActivity(i);
 		
 	}
 }
