3d6429c4fb9ef7616c8f91c3462fdfd601ded39e
Mauro Bahamondes <maurobahamondes@gmail.com>
2013-01-30 15:28:53 -0300

    activiti class remove import no usados.

--- src/cl/imc/codigo/ImcActivity.java
+++ src/cl/imc/codigo/ImcActivity.java
@@ -8,7 +8,6 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class ImcActivity extends Activity {
 
